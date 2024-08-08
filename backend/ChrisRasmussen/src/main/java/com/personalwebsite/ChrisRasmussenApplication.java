0c1fdf1c6d267881a200951bc9d2f248c9f76321
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2024-08-08 03:58:33 +0200

    security changes

--- backend/ChrisRasmussen/src/main/java/com/personalwebsite/ChrisRasmussenApplication.java
+++ backend/ChrisRasmussen/src/main/java/com/personalwebsite/ChrisRasmussenApplication.java
@@ -2,12 +2,21 @@ package com.personalwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ChrisRasmussenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChrisRasmussenApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder configurePasswordEncoder() {
+		return new BCryptPasswordEncoder();
+
 	}
 
 }
